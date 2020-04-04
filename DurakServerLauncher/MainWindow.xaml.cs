3963a2ccf73187a808d794ba535a3c9a97e8bb08
Maor Gershman <mgmaorvids@gmail.com>
2020-04-04 12:43:15 +0300

    Added seed checking for the launcher

--- DurakServerLauncher/MainWindow.xaml.cs
+++ DurakServerLauncher/MainWindow.xaml.cs
@@ -27,10 +27,39 @@ namespace DurakServerLauncher
 
         private void GenerateRandomSeed()
         {
-            var random = new Random();
-            uint number = (uint)(random.Next(int.MinValue, int.MaxValue) + (uint)int.MaxValue);
-            textBoxDeckSeed.Text = "0x" + Convert.ToString(number, 16);
+            uint number = (uint)(new Random().Next(int.MinValue, int.MaxValue) + (uint)int.MaxValue);
+            textBoxDeckSeed.Text = "0x" + Convert.ToString(number, 16).PadLeft("ffffffff".Length, '0');
         }
+
+        private bool CheckSeed()
+        {
+            var seed = textBoxDeckSeed.Text.ToLower();
+            
+            if (seed.Length != "0xffffffff".Length)
+            {
+                return false;
+            }
+
+            if (!(seed[0] == '0' && seed[1] == 'x'))
+            {
+                return false;
+            }
+
+            for (int i = "0x".Length; i < seed.Length; i++)
+            {
+                char c = seed[i];
+                if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f')))
+                {
+                    return false;
+                }
+            }
+
+            textBoxDeckSeed.Text = seed; // Switch all to lower case
+            
+            return true;
+        }
+
+        //
 
         private void windowLauncher_Loaded(object sender, RoutedEventArgs e)
         {
@@ -40,6 +69,19 @@ namespace DurakServerLauncher
         private void buttonGenerateNewSeed_Click(object sender, RoutedEventArgs e)
         {
             GenerateRandomSeed();
+        }
+
+        private void buttonRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckSeed())
+            {
+                MessageBox.Show("Invalid seed!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Preparing to launch!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                // TODO!
+            }
         }
     }
 }
